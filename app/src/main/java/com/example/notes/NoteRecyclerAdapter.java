d21fb9b1f680d5a0489118e6a5c8db39462661ff
Sommie09 <ceekay2a7@gmail.com>
2020-06-24 23:21:00 +0100

    Implemented Recycler View

--- app/src/main/java/com/example/notes/NoteRecyclerAdapter.java
+++ app/src/main/java/com/example/notes/NoteRecyclerAdapter.java
@@ -1,13 +1,57 @@
 package com.example.notes;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class NoteRecyclerAdapter {
+import java.util.List;
+
+public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder> {
+    private final Context mContext;
+    private final LayoutInflater mLayoutInflater;
+    private final List<NoteInfo> mNotes;
+
+    public NoteRecyclerAdapter(Context context, List<NoteInfo> notes) {
+        mContext = context;
+        mNotes = notes;
+        mLayoutInflater = LayoutInflater.from(mContext);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mLayoutInflater.inflate(R.layout.item_note_list, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        NoteInfo note = mNotes.get(position);
+        holder.mTextCourse.setText(note.getCourse().getTitle());
+        holder.mTextTitle.setText(note.getTitle());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNotes.size();
+    }
+
     public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public final TextView mTextCourse;
+        public final TextView mTextTitle;
+
         public ViewHolder(View itemView){
             super(itemView);
+            mTextCourse = itemView.findViewById(R.id.text_course);
+            mTextTitle = itemView.findViewById(R.id.text_title);
         }
     }
 }
